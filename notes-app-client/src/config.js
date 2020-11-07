82099424dd85c245543f89c703ac8f15f8ffc889
danielmbutler <danielmbutler@hotmail.co.uk>
2020-11-07 18:51:12 +0000

    Serverless login app with cognito

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-db"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://0ukgbzh17d.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_Yu1lAFBKD",
+      APP_CLIENT_ID: "7rqokfv7nklg5tsoopqafsqeus",
+      IDENTITY_POOL_ID: "us-east-1:676a0f19-fdde-4a39-a112-e5e58c2c5166"
+    }
+  };
