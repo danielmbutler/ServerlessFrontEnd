098b277b1f178f41593ef480ad6e8025617cf5da
danielmbutler <danielmbutler@hotmail.co.uk>
2020-11-09 13:27:57 +0000

    Added Signup Page

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
   return (
@@ -11,7 +12,10 @@ export default function Routes() {
         <Home />
       </Route>
       <Route exact path="/login">
-         <Login />
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
       </Route>
       {/* Finally, catch all unmatched routes */}
     <Route>
